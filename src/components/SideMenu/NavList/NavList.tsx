45819392709f6489f6194a56476b1caf1fa95250
oyasumipants <ibaraki_keigo@cyberagent.co.jp>
2024-10-17 10:56:57 +0900

    feat: add link list

---
+++ src/components/SideMenu/NavList/NavList.tsx
@@ -0,0 +1,11 @@
+const NavList = () => {
+  return (
+    <div>
+        <div>Link1</div>
+        <div>Link2</div>
+        <div>Link3</div>
+    </div>
+  )
+}
+
+export default NavList
---
+++ src/components/SideMenu/SideMenu.tsx
@@ -0,0 +1,10 @@
+const SideMenu = () => {
+  return <div>
+        <div>
+            <h1>Next Tasks</h1>
+            
+        </div>
+    </div>;
+};
+
+export default SideMenu

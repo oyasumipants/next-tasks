bda08132892167b4b86ace4467218d22fdb25245
oyasumipants <ibaraki_keigo@cyberagent.co.jp>
2024-10-28 01:29:49 +0900

    feat: add class icon

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -1,8 +1,10 @@
+import NavList from "./NavList/NavList";
+
 const SideMenu = () => {
-  return <div>
+  return <div className='w-56 pt-8 bg-gray-800 text-white'>
         <div>
-            <h1>Next Tasks</h1>
-            
+            <h1 className='px-4 text-2xl font-bold'>Next Tasks</h1>
+            <NavList />
         </div>
     </div>;
 };
